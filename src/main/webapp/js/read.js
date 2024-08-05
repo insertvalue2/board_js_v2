addd2c9028f3e21e2f4c509ecead90d4c3111a35
kdt4 <insertvaue@naver.com>
2024-08-05 17:39:03 +0900

    read.js 코드 추가

--- src/main/webapp/js/read.js
+++ src/main/webapp/js/read.js
@@ -5,12 +5,20 @@ const username = document.getElementById("username");
 const imgBox = document.querySelector(".img-box");
 const content = document.getElementById("content");
 
-const boardList = JSON.parse(localStorage.getItem("boardList"));
-const page = localStorage.getItem("page");
+const boardList = JSON.parse(localStorage.getItem("boardList")) || [];
+const page = localStorage.getItem("page") || 0;
+const button = document.querySelector(".btn");
 
+// todo 수정 예정
+const userCode = localStorage.getItem("user");
 
+console.log("userCode", userCode);
 
 function load() {
+    if (!boardList.length || !boardList[page]) {
+        console.error("Invalid board list or page.");
+        return;
+    }
 
     const board = boardList[page];
     if (board.img != null) {
@@ -26,10 +34,33 @@ function load() {
     board.count = board.count + 1;
 
     // 기존의 게시물을 업데이트된 게시물로 교체
-    // splice 함수는 배열에서 요소를 제거하거나 교체할 때 사용
-    // 여기서는 page 인덱스의 요소를 1개 제거하고, 업데이트된 board로 교체
     boardList.splice(page, 1, board);
-    localStorage.setItem("board", JSON.stringify(boardList));
+    localStorage.setItem("boardList", JSON.stringify(boardList));
 }
 
+function addEventListeners() {
+    button.addEventListener('click', modifyPost);
+}
+
+function modifyPost() {
+    if (!userCode || userCode === "null") {
+        alert("로그인이 필요합니다.");
+        location.href = "signin.html";
+        return;
+    }
+
+    // 로그인 사용자와 글쓴이가 같은지 체크
+    // 로직 스스로 만들어 보기 
+    const user = getUserByUserCode(userCode);
+   
+    location.href = "modify.html";
+}
+
+function getUserByUserCode(userCode) {
+    const userListHeader = JSON.parse(localStorage.getItem("userListHeader")) || {};
+    return userListHeader[userCode];
+}
+
+// 초기화 함수 호출
 load();
+addEventListeners();
